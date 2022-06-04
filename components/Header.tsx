fc43600b6e8d50102910ec428bad77cbdee9d12d
revoyrm <makr28@gmail.com>
2022-06-04 19:07:52 -0400

    initial

---
+++ components/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import type { ReactElement } from "react";
+
+const Header = (): ReactElement => {
+  return (
+    <div className="w-full">
+      <div>
+        <h1>Author</h1>
+      </div>
+      <hr />
+    </div>
+  );
+};
+
+export default Header;
---
+++ pages/Landing.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import type { ReactElement } from "react";
+import Header from "../components/Header";
+
+const Landing = (): ReactElement => {
+  return (
+    <div>
+      <Header />
+      <div>YO YO YO</div>
+    </div>
+  );
+};
+
+export default Landing;
