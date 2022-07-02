e413149c401623270e56c0066853d4d7ce1b67a6
revoyrm <makr28@gmail.com>
2022-07-02 18:25:08 -0400

    lint and ts

--- components/BookCard.tsx
+++ components/BookCard.tsx
@@ -1,14 +1,35 @@
-import React from "react";
-import type { Book } from "./types";
+import type { ReactElement } from 'react';
+import React from 'react';
 
-const BookCard = ({ id, title, author, summary, notes }: Book) => {
+import type { Book } from './types';
+
+export function BookCard({
+  id,
+  title,
+  author,
+  summary,
+  notes,
+}: Book): ReactElement {
   return (
-    <div className="border">
-      <h2>{`${title} - ${author}`}</h2>
-      <hr />
-      <h3>{summary}</h3>
+    <div
+      className="
+        hover:border-3 
+        h-52 
+        w-60 
+        rounded-lg 
+        border-2
+        border-primary
+        bg-secondary
+        p-2
+        text-primary"
+      role="button"
+      tabIndex={0}
+      onClick={() => {}}
+      onKeyDown={() => {}}
+    >
+      <h2 className="mb-1 font-bold">{`${title} - ${author ?? ''}`}</h2>
+      <hr className="mb-1" />
+      <p>{summary}</p>
     </div>
   );
-};
-
-export default BookCard;
+}
--- components/BookResults.tsx
+++ components/BookResults.tsx
@@ -1,71 +1,70 @@
-import { ReactElement } from "react";
-import BookCard from "./BookCard";
+import type { ReactElement } from 'react';
+
+import { BookCard } from './BookCard';
 
 const labels = {
-  setting: "setting",
-  character: "character",
-  chapter: "chapter",
+  setting: 'setting',
+  character: 'character',
+  chapter: 'chapter',
 };
 
 const books = [
   {
     id: 1,
-    title: "Tarsier",
-    author: "Fay Avery",
-    summary: "kids in space",
+    title: 'Tarsier',
+    author: 'Fay Avery',
+    summary: 'kids in space',
     notes: [
       {
-        label: "setting",
-        note: "the dawn",
+        label: 'setting',
+        note: 'the dawn of blah and time and 42 and such',
       },
       {
-        label: "character",
-        note: "Alya",
+        label: 'character',
+        note: 'Alya',
       },
     ],
   },
   {
     id: 2,
-    title: "Spirit",
-    author: "Fay Avery",
-    summary: "kids in space",
+    title: 'Spirit',
+    author: 'Fay Avery',
+    summary: 'kids in space',
     notes: [
       {
-        label: "setting",
-        note: "Calister Isle",
+        label: 'setting',
+        note: 'Calister Isle',
       },
       {
-        label: "character",
-        note: "Arya",
+        label: 'character',
+        note: 'Arya',
       },
     ],
   },
   {
     id: 3,
-    title: "Tally hoe",
-    author: "Fay Avery",
-    summary: "whatever",
+    title: 'Tally hoe',
+    author: 'Fay Avery',
+    summary: 'whatever you want to happen, happens. an on demand circus',
     notes: [
       {
-        label: "setting",
-        note: "the dusk",
+        label: 'setting',
+        note: 'the dusk',
       },
       {
-        label: "character",
-        note: "ayla",
+        label: 'character',
+        note: 'ayla',
       },
     ],
   },
 ];
 
-const BookResults = (): ReactElement => {
+export function BookResults(): ReactElement {
   return (
-    <section className="h-3/4 flex justify-between p-20">
+    <section className="flex h-3/4 justify-between p-20">
       {books.map((book) => (
         <BookCard key={book.id} {...book} />
       ))}
     </section>
   );
-};
-
-export default BookResults;
+}
