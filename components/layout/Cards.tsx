2ce82452162a3b7f47299995f234d3c7441a9909
revoyrm <makr28@gmail.com>
2023-01-16 17:39:47 -0500

    grid for cards layout

--- components/layout/Cards.tsx
+++ components/layout/Cards.tsx
@@ -1,10 +1,11 @@
-import type { ReactElement } from 'react';
+import type { ReactElement, RefObject } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 type CardsProps = { children: ReactElement[] };
 
 export function Cards({ children }: CardsProps): ReactElement {
   return (
-    <section className="m-4 mx-auto flex h-fit max-w-4xl flex-wrap justify-evenly">
+    <section className="m-4 grid h-fit w-fit justify-evenly bg-[red] md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       {children}
     </section>
   );
